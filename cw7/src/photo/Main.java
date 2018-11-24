4a1f3e667f27d084b3aa2765d1e10d3f7f9cb0a9
Jakub Ficoń <kuba.ficon23@gmail.com>
2018-11-24 14:11:58 +0100

    zdjecia

---
+++ cw7/src/photo/Main.java
@@ -0,0 +1,31 @@
+package photo;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.DirectoryChooser;
+import javafx.stage.Stage;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("People");
+        primaryStage.setScene(new Scene(root, 1000, 600));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+
+        launch(args);
+    }
+}
