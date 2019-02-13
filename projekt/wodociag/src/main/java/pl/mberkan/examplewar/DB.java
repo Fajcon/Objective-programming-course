be21076173740b908462b8db8fee8d418c86ea87
Fajcon <kuba.ficon23@gmail.com>
2019-02-13 12:56:24 +0100

    warnings

--- projekt/wodociag/src/main/java/pl/mberkan/examplewar/DB.java
+++ projekt/wodociag/src/main/java/pl/mberkan/examplewar/DB.java
@@ -1,6 +1,7 @@
 package pl.mberkan.examplewar;
 
 import java.sql.*;
+import java.util.ArrayList;
 
 public class DB {
     private Connection conn = null;
@@ -100,15 +101,15 @@ public class DB {
         return null;
 
     }
-    public Node getNextNode(String id){
+    public ArrayList<Node> getNextNode(String id){
         connect();
-        Node result = null;
+        ArrayList<Node> result =new ArrayList<>();
 
         try {
             stmt = conn.createStatement();
             rs = stmt.executeQuery("SELECT * FROM punkty WHERE PrevID ="+'\"'+id+'\"');
             while(rs.next()){
-                result = new Node(rs.getString(1),rs.getDouble(2),rs.getDouble(3),rs.getDouble(4),rs.getDouble(5),rs.getString(6),rs.getString(7));
+                result.add(new Node(rs.getString(1),rs.getDouble(2),rs.getDouble(3),rs.getDouble(4),rs.getDouble(5),rs.getString(6),rs.getString(7)));
             }
             stmt.close();
             conn.close();
@@ -210,13 +211,15 @@ public class DB {
         float water1 = 0;
         float water2 = 0;
         String prevID = prevID(id);
+        ArrayList<Node> nodes = getNextNode(prevID);
         connect();
         try {
             stmt = conn.createStatement();
-            rs = stmt.executeQuery("SELECT WaterIn FROM woda WHERE ID ="+'\"'+id+'\"');
-
-            while(rs.next() ){
-                water1=rs.getFloat(1);
+            for (Node n:nodes) {
+                rs = stmt.executeQuery("SELECT WaterIn FROM woda WHERE ID =" + '\"' + n.getID()+ '\"');
+                while(rs.next()) {
+                    water1 += rs.getFloat(1);
+                }
             }
             stmt.close();
             stmt = conn.createStatement();
